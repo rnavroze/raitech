ce298ace6df049aaaf77cb8bab79db7db724ebe8
Raihan <rnavroze@gmail.com>
2020-05-08 23:40:43 +0530

    Add trial projects page with tabs.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimationSmall } from '../fade.animation';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
+  animations: [fadeAnimationSmall],
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  selectedTab: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  animateTabChange(event) {
+    this.selectedTab = event;
+  }
+
+  getCurrentTab() {
+    return this.selectedTab;
+  }
 }
